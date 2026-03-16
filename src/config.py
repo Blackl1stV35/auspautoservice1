c14205fc9565130b9016848c1e145ffd3dced4fb
kamolpeen <101030487+kamolpeen@users.noreply.github.com>
2026-03-16 13:45:42 +0700

    update: new Line and Groq API keys

--- src/config.py
+++ src/config.py
@@ -17,8 +17,13 @@ logger = logging.getLogger(__name__)
 # Load environment variables
 load_dotenv()
 
+# Database Keys
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+# API Keys for Bot & NLP
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Missing Supabase credentials in .env file")
